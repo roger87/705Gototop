28a4d25d399da678680d5de9d7e37c9d44cbbb22
roger87 <willy32136@gmail.com>
2020-07-12 12:56:10 +0800

    加上指定元素

--- gototop.js
+++ gototop.js
@@ -1,13 +1,16 @@
 window.onload =() => {
-    console.log("頁面載入完成");
-
+   
     var test = document.getElementById("test");
 
     test.onclick = () => {
         var target = test.getAttribute("data-gte-target");
         var offset = test.getAttribute("data-gte-offset");
 
-        console.log(target);
-        console.log(offset);
+        var element = document.getElementById(target);
+
+        window.scrollTo({
+            top: element.offsetTop,
+            behavior:"smooth"
+        })
     }
 }
